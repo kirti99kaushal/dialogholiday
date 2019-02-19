1b38fddd660c1f91d7cf5e7e679d29767bda8533
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-02-19 16:06:35 +0530

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -20,19 +20,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 
 #from flask import Flask, render_template, flash, request,jsonify
 
-
-
-class BaseModel(db.Model):
-"""Base data model for all objects"""
-__abstract__ = True
-    # define here __repr__ and json methods or any common method
-    # that you need for all your models
-
-css YourModel(BaseModel):
-"""modelal for one of your table"""
-    __tablename__ = 'holidays'
-    # define your model
-
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
